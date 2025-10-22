e682e63b9075454d3eec6f42512be397590ee703
Tanmay Bhadauria <145855521+Tan28bha@users.noreply.github.com>
2025-10-23 01:27:26 +0530

    23rd october

---
+++ TYPESCRIPT/Intro.ts
@@ -0,0 +1,5 @@
+let message: string = "Hello Tanmay!";
+console.log(message);
+
+let hero: string ="My name is tanmya BHAAURIA"
+console.log(hero);
---
+++ TYPESCRIPT/intro.js
@@ -0,0 +1,5 @@
+var message = "Hello Tanmay!";
+console.log(message);
+
+var hero = "My name is tanmya BHAAURIA";
+console.log(hero);
